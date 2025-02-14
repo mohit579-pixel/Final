650ecfdf500af2775a10e78487dc4f494c2fa60f
tanishq-khandelwal <tanishqkhandelwal2019@gmail.com>
2025-02-14 20:57:59 +0530

    Added Shadcn and tailwind v 3.4.1

---
+++ Final/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+ 
+
+  return (
+    <>
+      React application
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
